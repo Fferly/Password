01c44b4d9e4dca7047340dd650ecbceae875e761
Andrii Skorokhod <andreyskorochod@gmail.com>
2023-10-17 19:48:16 +0300

    statusbar component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PassfieldComponent } from './passfield/passfield.component';
+import { StatusbarComponent } from './statusbar/statusbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PassfieldComponent,
+    StatusbarComponent,
   ],
   imports: [
     BrowserModule
